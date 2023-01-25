a0d98013a608336f8e4e70ae1c04e00add48b526
Vivu24 <93614868+Vivu24@users.noreply.github.com>
2023-01-25 19:05:39 +0100

    Actualización del Movimiento y CharacterController
    Hemos estado toda la tarde trabajando sobre el antiguo movimiento para acabar usando el resultado final, hemos quitado el input para poner el character controller

--- Assets/Scripts/MovementComponent.cs
+++ Assets/Scripts/MovementComponent.cs
@@ -4,16 +4,41 @@ using UnityEngine;
 
 public class MovementComponent : MonoBehaviour
 {
-    [SerializeField]
-    private GameObject _myPlayer;
-    [SerializeField]
-    private CharacterController _characterController;
-    [SerializeField]
-    private float _speed;
 
-    public void Walk(Vector2 move)     //Método para andar, se pasa como parámetro hacia donde moverse
-    {            
-        _characterController.Move(move * Time.deltaTime * _speed);
+    public CharacterController2D controller;
+
+    public float runSpeed = 40f;
+
+    float horizontalMove = 0f;
+    bool jump = false;
+    bool crouch = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        horizontalMove = Input.GetAxis("Horizontal") * runSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            crouch = true;
+        }
+        else if (Input.GetKeyUp(KeyCode.S))
+        {
+            crouch = false;
+        }
+
     }
 
+    void FixedUpdate()
+    {
+        // Move our character
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
+    }
 }
